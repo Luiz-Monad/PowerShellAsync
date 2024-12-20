2fd31f4d868ae5726bada6fdf3a5f638e3400851
Luiz Felipe Stangarlin <luizfelipestang@gmail.com>
2024-12-20 17:05:28 -0600

    chore!: upgrade project to dotnet8

--- PowerShellAsyncExample/PowerShellAsyncExample/MultiSqlCmdlet.cs
+++ PowerShellAsyncExample/PowerShellAsyncExample/MultiSqlCmdlet.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using System.Management.Automation;
 using System.Threading.Tasks;
-using JetBrains.Annotations;
 using TTRider.PowerShellAsync;
 
 namespace PowerShellAsyncExample
